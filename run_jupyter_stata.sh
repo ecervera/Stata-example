0e2cb4e54d97b123e481a24e2e53858956381dc1
Enric Cervera <ecervera@uji.es>
2024-01-16 22:48:53 +0100

    Updata name of image

--- run_jupyter_stata.sh
+++ run_jupyter_stata.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --name jupyter-stata \
+docker run -it --rm --name robinlab/jupyter-stata:latest \
   -p 8888:8888 \
   -v $(pwd):/project \
   -v $(pwd)/stata.lic:/usr/local/stata/stata.lic \
