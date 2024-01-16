aa202279f4b84b82269580ce4ac623ffd279ed54
Enric Cervera <ecervera@uji.es>
2024-01-16 22:51:36 +0100

    Fix name

--- run_jupyter_stata.sh
+++ run_jupyter_stata.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --name robinlab/jupyter-stata:latest \
+docker run -it --rm --name jupyter-stata \
   -p 8888:8888 \
   -v $(pwd):/project \
   -v $(pwd)/stata.lic:/usr/local/stata/stata.lic \
-  jupyter-stata
+  robinlab/jupyter-stata:latest
